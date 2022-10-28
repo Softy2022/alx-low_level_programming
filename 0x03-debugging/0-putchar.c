1db64f3f4df9c44563a73d0988b106f1f229b74f
Softy2022 <jolayemiolumide@yahoo.com>
2022-10-27 17:31:57 -0700

    ok

---
+++ 0x03-debugging/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#include "main.h"
+
+
+/**
+  *
+  *   * main - Entry point of the program"
+  *
+  *     *
+  *
+  *       * Return: Always returns 0(success)
+  *
+  */
+
+int main(void)
+
+{
+
+		printf("_putchar\n");
+		return (0);
+
+}

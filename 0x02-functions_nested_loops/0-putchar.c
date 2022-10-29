56bb38e6e68fae07401d067986b48dc9ec8463b7
Softy2022 <jolayemiolumide@yahoo.com>
2022-10-28 21:58:35 -0700

    pushing

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,9 +2,9 @@
 
 #include "main.h"
 
-/**  
-  * main - Entry point of the program"   
-  * Return: Always returns 0(success)
+/**
+  *main - Entry point of the program"   
+  *Return: Always returns 0(success)
   */
 
 int main(void)

8c9f650938a4e5dbdb0ff92f7c7b1f212829a637
Softy2022 <jolayemiolumide@yahoo.com>
2022-10-30 20:14:36 -0700

    task

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-  * main - prints alphabet in lowercase
+  * main - prints the lowercase alphabet in reverse,
   * followed by a new line
   * Return: Always 0 (Success)
   */
@@ -9,12 +9,12 @@
 int main(void)
 {
 	char ch;
-
-	for (ch = 'a'; ch <= 'z'; ch++)
+	for (ch = 'z'; ch >= 'a'; ch--)
 	{
 		putchar(ch);
 	}
 	putchar('\n');
 
 	return (0);
+
 }
